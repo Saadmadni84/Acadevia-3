62318a4ccc1eb7dbb4461d0e3e1c19ddb4e69c9c
Saad Madni <saadmadni84@gmail.com>
2025-09-18 19:35:19 +0530

    "initial"

--- .next/static/chunks/fallback/webpack.js
+++ .next/static/chunks/fallback/webpack.js
@@ -91,6 +91,39 @@
 /******/ 		};
 /******/ 	}();
 /******/ 	
+<<<<<<< HEAD:.next/static/chunks/fallback/webpack.js
+=======
+/******/ 	/* webpack/runtime/create fake namespace object */
+/******/ 	!function() {
+/******/ 		var getProto = Object.getPrototypeOf ? function(obj) { return Object.getPrototypeOf(obj); } : function(obj) { return obj.__proto__; };
+/******/ 		var leafPrototypes;
+/******/ 		// create a fake namespace object
+/******/ 		// mode & 1: value is a module id, require it
+/******/ 		// mode & 2: merge all properties of value into the ns
+/******/ 		// mode & 4: return value when already ns object
+/******/ 		// mode & 16: return value when it's Promise-like
+/******/ 		// mode & 8|1: behave like require
+/******/ 		__webpack_require__.t = function(value, mode) {
+/******/ 			if(mode & 1) value = this(value);
+/******/ 			if(mode & 8) return value;
+/******/ 			if(typeof value === 'object' && value) {
+/******/ 				if((mode & 4) && value.__esModule) return value;
+/******/ 				if((mode & 16) && typeof value.then === 'function') return value;
+/******/ 			}
+/******/ 			var ns = Object.create(null);
+/******/ 			__webpack_require__.r(ns);
+/******/ 			var def = {};
+/******/ 			leafPrototypes = leafPrototypes || [null, getProto({}), getProto([]), getProto(getProto)];
+/******/ 			for(var current = mode & 2 && value; typeof current == 'object' && !~leafPrototypes.indexOf(current); current = getProto(current)) {
+/******/ 				Object.getOwnPropertyNames(current).forEach(function(key) { def[key] = function() { return value[key]; }; });
+/******/ 			}
+/******/ 			def['default'] = function() { return value; };
+/******/ 			__webpack_require__.d(ns, def);
+/******/ 			return ns;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+>>>>>>> 396f503d ("initial"):frontend/.next/static/chunks/webpack.js
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	!function() {
 /******/ 		// define getter functions for harmony exports
@@ -103,6 +136,31 @@
 /******/ 		};
 /******/ 	}();
 /******/ 	
+<<<<<<< HEAD:.next/static/chunks/fallback/webpack.js
+=======
+/******/ 	/* webpack/runtime/ensure chunk */
+/******/ 	!function() {
+/******/ 		__webpack_require__.f = {};
+/******/ 		// This file contains only the entry chunk.
+/******/ 		// The chunk loading function for additional chunks
+/******/ 		__webpack_require__.e = function(chunkId) {
+/******/ 			return Promise.all(Object.keys(__webpack_require__.f).reduce(function(promises, key) {
+/******/ 				__webpack_require__.f[key](chunkId, promises);
+/******/ 				return promises;
+/******/ 			}, []));
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/get javascript chunk filename */
+/******/ 	!function() {
+/******/ 		// This function allow to reference async chunks
+/******/ 		__webpack_require__.u = function(chunkId) {
+/******/ 			// return url for filenames based on template
+/******/ 			return undefined;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+>>>>>>> 396f503d ("initial"):frontend/.next/static/chunks/webpack.js
 /******/ 	/* webpack/runtime/get javascript update chunk filename */
 /******/ 	!function() {
 /******/ 		// This function allow to reference all chunks
@@ -120,10 +178,14 @@
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
 <<<<<<< HEAD:.next/static/chunks/fallback/webpack.js
+<<<<<<< HEAD:.next/static/chunks/fallback/webpack.js
 /******/ 		__webpack_require__.h = function() { return "b84f96fee79b25c8"; }
 =======
 /******/ 		__webpack_require__.h = function() { return "2b99c8d233e1c6b6"; }
 >>>>>>> 0022d79d (initial):frontend/.next/static/chunks/webpack.js
+=======
+/******/ 		__webpack_require__.h = function() { return "dc4a02675dd09e4e"; }
+>>>>>>> 396f503d ("initial"):frontend/.next/static/chunks/webpack.js
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
